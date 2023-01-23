99f50e1a1bca440338e8c6e7cf1d4300b254c7a8
maorre <67946243+maorre@users.noreply.github.com>
2023-01-23 12:19:54 +0200

    Commit message

---
+++ index.js
@@ -0,0 +1,27 @@
+const add = (a, b) => {
+    return a + b;
+  };
+  
+  const sub = (a, b) => {
+    return a - b;
+  };
+  const mul = (a, b) => {
+    return a * b;
+  };
+  
+  const div = (a, b) => {
+    return a / b;
+  };
+  
+  const square = (a) => {
+    return Math.sqrt(a);
+  };
+  
+  const power = (a, b) => {
+    return Math.pow(a, b);
+  };
+  
+  const mod = (a, b) => {
+    return a % b;
+  };
+  
